b0b7de34a02b544a484e81edde184246f494fb58
André Samuelsson <andresamuelsson94@gmail.com>
2017-06-14 14:51:39 +0200

    Use CORS

--- backend/main.go
+++ backend/main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"log"
 	"net/http"
+	"github.com/rs/cors"
 )
 
 func listHandler(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
-	w.Write([]byte("This is an example server.\n"))
+	w.Write([]byte("List requested.\n"))
 }
 
 func adminHandler(w http.ResponseWriter, req *http.Request) {
@@ -16,12 +17,15 @@ func adminHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", listHandler)
-	http.HandleFunc("/list", listHandler)
-	http.HandleFunc("/admin", adminHandler)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", listHandler)
+	mux.HandleFunc("/list", listHandler)
+	mux.HandleFunc("/admin", adminHandler)
+
+	handler := cors.Default().Handler(mux)
 
 	log.Printf("About to listen on 3001. Go to http://127.0.0.1:3001/")
 	//err := http.ListenAndServeTLS(":3001", "cert.pem", "key.pem", nil)
-	err := http.ListenAndServe(":3001", nil)
+	err := http.ListenAndServe(":3001", handler)
 	log.Fatal(err)
 }
