ff57d3e36875dc463a412ae95296542ada476ba8
André Samuelsson <andresamuelsson94@gmail.com>
2017-06-25 17:59:05 +0200

    Add react-router and list component to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import List from './List.js'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+
+                <div className="App">
+                    <div className="App-header">
+                        <img src={logo} className="App-logo" alt="logo" />
+                        <h2>Welcome to React</h2>
+                    </div>
+                    <Route path="/list" component={List}/>
+                </div>
+
+
+            </Router>
+        );
+    }
 }
 
 export default App;
