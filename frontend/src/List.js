34d4bd54621bb9f7c31b941d8ddf125948fa7dfa
Kalior <kalioragus@gmail.com>
2017-06-30 18:19:35 +0200

    Initial List work

--- frontend/src/List.js
+++ frontend/src/List.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
 import { toast } from "react-toastify";
+import styled from "styled-components";
 import "whatwg-fetch";
 
 import { getJson, postJson, sendDelete } from "./fetch.js";
+
+import {
+  Container,
+  Title,
+  Row,
+  RowContent,
+  SubmitButton
+} from "./SharedComponents.js";
 
 class List extends Component {
   constructor(props) {
@@ -66,36 +75,92 @@ class List extends Component {
     let { status, speakerLists } = this.state;
     console.log(status);
     return (
-      <div>
-        <input
-          type="submit"
-          value={
-            status === this.statics.notRegistered
-              ? "I want to talk"
-              : "I don't want to talk"
-          }
-          onClick={
-            status === this.statics.notRegistered
-              ? () => this.registerTalkRequest()
-              : () => this.unregisterTalkRequest()
-          }
-        />
-        <div>
-          {speakerLists.map(list => {
-            return (
-              <div key={list.id}>
-                <h3>{list.title}</h3>
-                {list.speakersQueue.map(user => {
-                  return <h5 key={user.id}>{user.nick}</h5>;
-                })}
-              </div>
-            );
-          })}
-        </div>
-
-      </div>
+      <Container>
+        <SubContainer>
+          <Row>
+            <RowContent>
+              <SubmitButton
+                type="button"
+                value={
+                  status === this.statics.notRegistered
+                    ? "I want to talk"
+                    : "I don't want to talk"
+                }
+                onClick={
+                  status === this.statics.notRegistered
+                    ? () => this.registerTalkRequest()
+                    : () => this.unregisterTalkRequest()
+                }
+              />
+            </RowContent>
+          </Row>
+          <ListsContainer>
+            {speakerLists.map(list => {
+              return (
+                <ListContainer key={list.id}>
+                  <ListHeader>
+                    <ListTitle>{list.title}</ListTitle>
+                  </ListHeader>
+                  <Title>First speaker list</Title>
+                  {list.speakersQueue.map(user => {
+                    return <Speaker key={user.id}>{user.nick}</Speaker>;
+                  })}
+                  <Title>Second speaker list</Title>
+                  {list.speakersQueue.map(user => {
+                    return <Speaker key={user.id}>{user.nick}</Speaker>;
+                  })}
+                </ListContainer>
+              );
+            })}
+          </ListsContainer>
+        </SubContainer>
+      </Container>
     );
   }
 }
+
+const ListHeader = styled.div`
+  height: 98px;
+  border: solid 1px #979797;
+  background-color: #efeeee;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ListTitle = styled.div`
+  font-family: Helvetica Neue,Helvetica,Roboto,Arial,sans-serif;
+  font-size: 2rem;
+  font-weight: bold;
+  color: #4a4a4a;
+  background-color: #efeeee;
+`;
+
+const SubContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  flex-direction: column;
+  width: 600px;
+`;
+
+const ListsContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+const ListContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  width: 300px;
+  background-color: #ffffff;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.5);
+`;
+
+const Speaker = styled.div`
+  font-size: 2rem;
+  margin-left: 68px;
+`;
 
 export default List;
