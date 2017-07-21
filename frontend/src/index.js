9041757d6af58ab6d8df6f3401d381b0c9bf88ee
Oscmage <oscar.evertsson@live.com>
2017-07-22 00:21:25 +0200

    Boooooooooooooorder-box

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { injectGlobal } from "styled-components";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// eslint-disable-next-line
+injectGlobal`
+    *, *:before, *:after {
+        box-sizing: border-box;
+    }
+`;
+
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
