f69b4651e5f124058d7833a0c462f413721a458c
Kalior <kalioragus@gmail.com>
2018-02-22 12:24:07 +0100

    Align columns by flex-start

--- frontend/src/SharedComponents.js
+++ frontend/src/SharedComponents.js
@@ -96,6 +96,7 @@ export const RowContainer = styled(Container)`
 export const ColumnContainer = styled(Container)`
   display: flex;
   flex-direction: column;
+  align-items: flex-start;
 `;
 
 export const SubListTitle = styled.div`
